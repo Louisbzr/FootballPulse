bc58a9ff820f7976fdc9030541b73c69e5e2dd8d
Louis Brazeau <louis.brazeau.pro@gmail.com>
2026-03-13 19:45:23 +0100

    first commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
+
+export default defineConfig({
+  plugins: [
+    react({
+      include: '**/*.{js,jsx}',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  define: {
+    'process.env': process.env,
+  },
+})
